cf36cdbb9261974b214add499deaa8d052a93e1b
Tobias Pfeiffer <pragtob@gmail.com>
2025-05-30 09:07:19 +0200

    include results

--- bench.exs
+++ bench.exs
@@ -19,3 +19,47 @@ Benchee.run(
   warmup: 2,
   memory_time: 2
 )
+
+# Operating System: Linux
+# CPU Information: AMD Ryzen 9 5900X 12-Core Processor
+# Number of Available Cores: 24
+# Available memory: 31.26 GB
+# Elixir 1.18.3
+# Erlang 27.3.2
+# JIT enabled: true
+
+# Benchmark suite executing with the following configuration:
+# warmup: 2 s
+# time: 3 s
+# memory time: 2 s
+# reduction time: 0 ns
+# parallel: 1
+# inputs: none specified
+# Estimated total run time: 28 s
+
+# Benchmarking ecto ...
+# Benchmarking inspect ...
+# Benchmarking to_sql ...
+# Benchmarking to_string ...
+# Calculating statistics...
+# Formatting results...
+
+# Name                ips        average  deviation         median         99th %
+# to_sql          10.47 M       95.53 ns ±41507.95%          40 ns          90 ns
+# to_string        8.03 M      124.51 ns ±42277.22%          50 ns         120 ns
+# inspect          1.75 M      569.90 ns  ±8174.08%         330 ns         870 ns
+# ecto            0.152 M     6559.75 ns   ±279.27%        6130 ns    11821.90 ns
+
+# Comparison:
+# to_sql          10.47 M
+# to_string        8.03 M - 1.30x slower +28.99 ns
+# inspect          1.75 M - 5.97x slower +474.37 ns
+# ecto            0.152 M - 68.67x slower +6464.22 ns
+
+# Memory usage statistics:
+
+# Name         Memory usage
+# to_sql              136 B
+# to_string           112 B - 0.82x memory usage -24 B
+# inspect             776 B - 5.71x memory usage +640 B
+# ecto              18640 B - 137.06x memory usage +18504 B
